d2f0348d343d97276e11e4064b9b3aebf2477898
DeyuanC <chendy2014@gmail.com>
2019-06-25 22:08:58 +0200

    my quicksort

---
+++ Algorithms/Sorting/myQuicksort.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+template<typename T>
+void quickSort(vector<T> &array, int left, int right)
+{
+
+    int l=left, r=right;
+    T pv = array[left],temp; //pivotalvalue
+
+  if (l<r) {
+    while (l<r) {
+      while (l<r&&array[r]>pv)
+        r--;
+      while (l<r&&array[l]<=pv){
+        l++;
+      }
+      if (l<r) {
+        temp=array[l];
+        array[l]=array[r];
+        array[r]=temp;
+      }
+    }
+    array[left]=array[l];
+    array[l]=pv;
+    quickSort(array,left,l-1);
+    quickSort(array,l+1,right);
+  }
+}
+
+int main()
+{
+	vector<double> array = {5,4,3,2,1};
+
+	quickSort(array,0,array.size()-1);
+  for(auto &p:array)
+    cout<<p<<" ";
+    cout<<endl;
+	return 0;
+}
